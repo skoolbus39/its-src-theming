17a51e86dae77dcdac2bbeabb2daf0f324bc1c12
Richard Lock <lock.j.h@gmail.com>
2018-11-28 14:08:12 -0600

    - completed form styling for UNK theme (#2)

--- assets/src/js/site.js
+++ assets/src/js/site.js
@@ -1,7 +1,13 @@
 $(
+		console.log($('[data-widget*="flatpickr"]')),
 		// initialize FlexNav
 		$(".flexnav").flexNav({
 			'hoverIntent': false,
-			'hoverIntentTimeout': 130
-		})
+			'hoverIntentTimeout': 130,
+		}),
+		$('[data-widget*="flatpickr"]').flatpickr({
+			enableTime: true,
+			dateFormat: "Y-m-d h:iK",
+		}),
+
 );
